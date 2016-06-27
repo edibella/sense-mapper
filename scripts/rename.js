7d9d38ea39df030fb1c84e845a3a18cd4f34246b
KC Erb <iamkcerb@gmail.com>
2016-06-27 11:28:28 -0600

    initial commit

---
+++ scripts/rename.js
@@ -0,0 +1,3 @@
+//Rename npm package so that matlab can use it
+fs = require('fs');
+fs.renameSync('../sense-mapper','../+SenseMapper')
---
+++ test.m
@@ -0,0 +1,4 @@
+% Basic command for testing self with MatlabSpec
+function test
+  MatlabSpec.run_tests('SenseMapper')
+end
